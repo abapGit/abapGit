016092b940c0752f55286867588d47c619ca0a9f
Lars Hvam <larshp@hotmail.com>
2016-07-02 10:41:44 +0200

    Update travis.sh

--- travis.sh
+++ travis.sh
@@ -4,3 +4,7 @@ wc -l ../zabapgit.abap
 cd ..
 git clone https://github.com/larshp/abapGit.git -b gh-pages pages
 ls -l
+cp zabapgit.abap pages/build/zabapgit$(date -u "+%Y-%m-%d-%H-%M-%S").abap
+cp zabapgit.abap pages/build/zabapgit.abap
+cd pages
+git status
