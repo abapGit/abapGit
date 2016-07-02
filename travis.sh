47c06aad655354064170fd6d88731e74a7ef03f8
Lars Hvam <larshp@hotmail.com>
2016-07-02 11:34:46 +0200

    use txt instead, allows for easier download

--- travis.sh
+++ travis.sh
@@ -4,12 +4,12 @@ wc -l ../zabapgit.abap
 cd ..
 git clone https://github.com/larshp/abapGit.git -b gh-pages pages
 ls -l
-cp zabapgit.abap pages/build/zabapgit$(date -u "+%Y-%m-%d-%H-%M-%S").abap
-cp zabapgit.abap pages/build/zabapgit.abap
+cp zabapgit.abap pages/build/zabapgit$(date -u "+%Y-%m-%d-%H-%M-%S").txt
+cp zabapgit.abap pages/build/zabapgit.txt
 cd pages
 git status
 git config --global user.email "builds@travis-ci.com"
 git config --global user.name "Travis CI"
-git add build/*.abap
+git add build/*.txt
 git commit -m "Travis build $TRAVIS_BUILD_NUMBER"
 git push -q https://$GITHUB_API_KEY@github.com/larshp/abapGit.git gh-pages > /dev/null 2>&1
