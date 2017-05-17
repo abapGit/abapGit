1d8a9b9c756d83d54f0546af9f155ee5c1a20122
Lars Hvam <larshp@hotmail.com>
2017-05-17 19:00:22 +0200

    close #755

--- travis.sh
+++ travis.sh
@@ -4,11 +4,11 @@ wc -l ../zabapgit.abap
 cd ..
 git clone https://github.com/larshp/abapGit.git -b build build
 ls -l
-cp zabapgit.abap build/zabapgit.txt
+cp zabapgit.abap build/zabapgit.abap
 cd build
 git status
 git config --global user.email "builds@travis-ci.com"
 git config --global user.name "Travis CI"
-git add zabapgit.txt
+git add zabapgit.abap
 git commit -m "Travis build $TRAVIS_BUILD_NUMBER"
 git push -q https://$GITHUB_API_KEY@github.com/larshp/abapGit.git build > /dev/null 2>&1
