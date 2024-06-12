abapGit/abapGit

commit 22e6df1d4dfb1c3a91a901a8a49d1acb56a7c04b

Remove duplicate clipboad action (#6944)

src/ui/routing/zcl_abapgit_gui_router.clas.abap
@@ -142,7 +142,7 @@ ENDCLASS.
 
 
 
-CLASS ZCL_ABAPGIT_GUI_ROUTER IMPLEMENTATION.
+CLASS zcl_abapgit_gui_router IMPLEMENTATION.
 
 
   METHOD abapgit_services_actions.
@@ -595,13 +595,7 @@ CLASS ZCL_ABAPGIT_GUI_ROUTER IMPLEMENTATION.
         lv_clip_content = ii_event->query( )->get( 'CLIPBOARD' ).
         APPEND lv_clip_content TO lt_clipboard.
         zcl_abapgit_ui_factory=>get_frontend_services( )->clipboard_export( lt_clipboard ).
-        MESSAGE 'Successfully exported URL to Clipboard.' TYPE 'S'.
-        rs_handled-state = zcl_abapgit_gui=>c_event_state-no_more_act.
-      WHEN zif_abapgit_definitions=>c_action-yank_to_clipboard.
-        lv_clip_content = ii_event->form_data( )->get( 'CLIPBOARD' ).
-        APPEND lv_clip_content TO lt_clipboard.
-        zcl_abapgit_ui_factory=>get_frontend_services( )->clipboard_export( lt_clipboard ).
-        MESSAGE 'Successfully exported to Clipboard.' TYPE 'S'.
+        MESSAGE 'Successfully exported to clipboard' TYPE 'S'.
         rs_handled-state = zcl_abapgit_gui=>c_event_state-no_more_act.
     ENDCASE.
 

src/ui/zabapgit_js_common.w3mi.data.js
@@ -1488,7 +1488,7 @@ LinkHints.prototype.handleKey = function(event) {
       this.displayHints(false);
       event.preventDefault();
       if (this.yankModeActive) {
-        submitSapeventForm({ clipboard: hint.parent.firstChild.textContent }, "yank_to_clipboard");
+        submitSapeventForm({ clipboard: hint.parent.firstChild.textContent }, "clipboard");
         this.yankModeActive = false;
       } else {
         this.hintActivate(hint);

src/zif_abapgit_definitions.intf.abap
@@ -374,7 +374,6 @@ INTERFACE zif_abapgit_definitions
       toggle_favorites              TYPE string VALUE 'toggle_favorites',
       url                           TYPE string VALUE 'url',
       where_used                    TYPE string VALUE 'where_used',
-      yank_to_clipboard             TYPE string VALUE 'yank_to_clipboard',
       zip_export                    TYPE string VALUE 'zip_export',
       zip_export_transport          TYPE string VALUE 'zip_export_transport',
       zip_import                    TYPE string VALUE 'zip_import',

commit 44321545587d77caee3a8ab92ac01811951e8ad5

Fix yank mode - copy to clipboad
regression from #6944

src/ui/routing/zcl_abapgit_gui_router.clas.abap
@@ -593,6 +593,13 @@ CLASS zcl_abapgit_gui_router IMPLEMENTATION.
         rs_handled-state = zcl_abapgit_gui=>c_event_state-no_more_act.
       WHEN zif_abapgit_definitions=>c_action-clipboard.
         lv_clip_content = ii_event->query( )->get( 'CLIPBOARD' ).
+        IF lv_clip_content IS INITIAL.
+          " yank mode sends via form_data
+          lv_clip_content = ii_event->form_data( )->get( 'CLIPBOARD' ).
+        ENDIF.
+        IF lv_clip_content IS INITIAL.
+          zcx_abapgit_exception=>raise( 'Export to clipboard failed, no data' ).
+        ENDIF.
         APPEND lv_clip_content TO lt_clipboard.
         zcl_abapgit_ui_factory=>get_frontend_services( )->clipboard_export( lt_clipboard ).
         MESSAGE 'Successfully exported to clipboard' TYPE 'S'.