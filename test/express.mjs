b20d89386b9585c19927d6dfb81d7004bc241986
Lars Hvam <5888506+larshp@users.noreply.github.com>
2026-07-08 13:10:22 +0200

    run more unit tests + update gitea (#7761)

--- test/express.mjs
+++ test/express.mjs
@@ -25,5 +25,30 @@ app.all(["/sap", "/sap*"], async function (req, res) {
   await cl_express_icf_shim.run({req, res, class: "ZCL_ABAPGIT_WEB_SICF"});
 });
 
-app.listen(PORT);
+const server = app.listen(PORT);
 console.log("Listening on port http://localhost:" + PORT + "/sap/zabapgit/");
+
+let shuttingDown = false;
+function shutdown(signal) {
+  if (shuttingDown) {
+    console.log("Forcing exit");
+    process.exit(1);
+  }
+  shuttingDown = true;
+  console.log("Received " + signal + ", shutting down, press again to force exit");
+  server.close(function (err) {
+    if (err) {
+      console.error(err.message);
+      process.exit(1);
+    }
+    process.exit(0);
+  });
+  // force exit if connections do not drain in time
+  setTimeout(function () {
+    console.log("Shutdown timed out, forcing exit");
+    process.exit(1);
+  }, 5000).unref();
+}
+
+process.on("SIGINT", () => shutdown("SIGINT"));
+process.on("SIGTERM", () => shutdown("SIGTERM"));
